4c44e626a0045c1cf83d694bf2183247135029d2
Ratko Gjurichanin <ratko.gjurichanin@gmail.com>
2020-03-07 14:36:58 -0600

    Getting values from the inputs in Object
    For now we are console logging them

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,20 @@
 import '../sass/main.scss';
-import budget from './budgetController';
-import UI from './uiController';
+import { getInput, elements } from './uiController';
+
+//1.Get the field input data
+const addItem = function() {
+  const input = getInput();
+  console.log(input);
+};
+
+//2.Add the item to the budget controller
+//3.Add the item to the UI
+//4.Calculate the budget
+//5.Display the budget on the UI
+
+elements.inputBtn.addEventListener('click', addItem);
+document.addEventListener('keydown', function(e) {
+  if (e.keyCode === 13) {
+    addItem();
+  }
+});
--- src/js/uiController.js
+++ src/js/uiController.js
@@ -1,1 +1,15 @@
-console.log('UITESTING');
+export const elements = {
+  inputType: document.querySelector('.budget__type'),
+  inputDesc: document.querySelector('.add__description'),
+  inputValue: document.querySelector('.add__value'),
+  inputBtn: document.querySelector('.add__btn')
+};
+
+export function getInput() {
+  elements;
+  return {
+    type: elements.inputType.value,
+    desc: elements.inputDesc.value,
+    value: elements.inputValue.value
+  };
+}
